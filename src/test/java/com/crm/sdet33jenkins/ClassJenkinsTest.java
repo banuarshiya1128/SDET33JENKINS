83f2d2b5d9489ae78765e38f8d02b05b6e25c8f8
hp <hp@HP>
2022-04-14 13:00:49 +0530

    FISRT COMMIT

---
+++ src/test/java/com/crm/sdet33jenkins/ClassJenkinsTest.java
@@ -0,0 +1,29 @@
+package com.crm.sdet33jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ClassJenkinsTest {
+	
+
+		@Test
+		public void testNGProgramsTest() {     //always method name should be in lowercase 
+			Reporter.log("test1", true);
+		}
+			@BeforeTest
+			public void testNGProgramsTest2() {
+				Reporter.log("test2", true);
+				
+			}	@AfterSuite
+				public void testNGProgramsTest3() {
+				System.out.println("@AfterSuite");
+					Reporter.log("test3", true);
+					
+			
+		}
+	}
+
+
+
